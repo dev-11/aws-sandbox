a0b5688aa4c4f3483bc84b0d38f8198b13fb6b74
Otto <otto@masterbranch.io>
2020-04-10 18:08:07 +0100

    bs4 code

--- entry_point/app.py
+++ entry_point/app.py
@@ -1,10 +1,34 @@
 import json
 from datetime import datetime as dt
+from bs4 import BeautifulSoup
+import requests
 
 
 def lambda_handler(event, context):
-    # TODO implement
+    data = get_section()
+
     return {
         'statusCode': 200,
-        'body': json.dumps(f'Hello from Lambda @ {dt.now()}!')
+        'body': json.dumps(f'Hello from Lambda @ {dt.now()}, {data}!')
     }
+
+
+def get_book_details(divs):
+    section = divs[0].find('h2').find('em').text
+    title = divs[1].find(class_='title').find('a').text
+    return section, title
+
+
+def get_section():
+    site = 'https://www.waterstones.com/campaign/books-of-the-month'
+    page = requests.get(site)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    lst = soup.find(class_='row home-row')
+    lst2 = lst.find_all(class_='span12')
+
+    bom = lst2[1:]
+    v = bom[0:2]
+
+    return get_book_details(v)
